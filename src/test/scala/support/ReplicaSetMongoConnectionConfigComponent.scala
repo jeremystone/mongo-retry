2c6cb5a1be3a4cb8c5e307d2e9cc7e25c3758464
jeremystone <6453521+jeremystone@users.noreply.github.com>
2020-09-15 13:58:08 +0100

    Fix up config

--- src/test/scala/support/ReplicaSetMongoConnectionConfigComponent.scala
+++ src/test/scala/support/ReplicaSetMongoConnectionConfigComponent.scala
@@ -2,6 +2,6 @@ package support
 
 trait  ReplicaSetMongoConnectionConfigComponent extends ConnectionConfigComponent {
   lazy val connectionConfig: ConnectionConfig = new ConnectionConfig {
-    def hosts = List(s"localhost:27017", s"localhost:27018", s"localhost:27019")
+    def hosts = List(s"mongo0:27017", s"mongo1:27018", s"mongo2:27019")
   }
 }
